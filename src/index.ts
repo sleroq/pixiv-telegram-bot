f5de3d12c226a4373cdf926f1574aba4a5fc7f88
sleeper_ok <sleeeper.ok@gmail.com>
2021-11-23 23:07:44 +0300

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv'
+import Werror from './lib/errors'
+import initBot from './bot/index'
+import setWebhook from './server'
+
+dotenv.config()
+
+const token = process.env.BOT_TOKEN
+if (!token) {
+	throw new Werror('Bot token not specified')
+}
+
+void (async() => {
+	// try {
+	// 	await connectToSQL()
+	// } catch (error) {
+	// 	throw new Werror(error, 'Connecting to the database')
+	// }
+
+	let bot
+	try {
+		bot = initBot(token)
+	} catch (error) {
+		throw new Werror(error, 'Initializing the bot')
+	}
+
+	const urlForWebhook = getUrlForWebhook()
+	if (urlForWebhook) {
+		try {
+			await setWebhook(bot, urlForWebhook)
+		} catch (error) {
+			throw new Werror(error, 'Starting with webhook')
+		}
+		console.log('Webhook is set')
+	} else {
+		console.log('Url for webhook not specified, starting polling')
+		void bot.start({
+			drop_pending_updates: true
+		})
+	}
+})()
+
+function getUrlForWebhook() {
+	if (process.env.HEROKU_APP_NAME) {
+		return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+	} else if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
+		return `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER.toLowerCase()}.repl.co`
+	}
+	return undefined
+}
