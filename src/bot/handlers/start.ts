a3fd63b7366ea0265406cfdac33dc38ecc409806
Sleroq <cantundo@pm.me>
2022-06-13 22:14:39 +0300

    Removed buttons for not yet implemented stuff

--- src/bot/handlers/start.ts
+++ src/bot/handlers/start.ts
@@ -3,23 +3,23 @@ const composer = new Composer()
 
 composer.command('start', async (ctx) => {
 	await ctx.reply(ctx.t('welcome'), {
-		disable_web_page_preview: true,
-		reply_markup: {
-			inline_keyboard: [
-				[
-					{
-						text: 'Русский',
-						callback_data: 'ru',
-					},
-				],
-				[
-					{
-						text: ctx.t('buttons.settings'),
-						callback_data: 'settings',
-					},
-				],
-			],
-		},
+		disable_web_page_preview: true, //,
+		// reply_markup: {
+		// 	inline_keyboard: [
+		// 		[
+		// 			{
+		// 				text: 'Русский',
+		// 				callback_data: 'ru',
+		// 			},
+		// 		],
+		// 		[
+		// 			{
+		// 				text: ctx.t('buttons.settings'),
+		// 				callback_data: 'settings',
+		// 			},
+		// 		],
+		// 	],
+		// },
 	})
 	await ctx.reply(ctx.t('found-bug'))
 })
