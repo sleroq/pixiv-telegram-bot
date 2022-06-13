886fdb89259253f5c490bd6d953e7bcb9e0357a4
Sleroq <cantundo@pm.me>
2022-06-13 12:46:44 +0300

    Fixed eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'google',
-  ],
-  rules: {
-    indent: ['error', 'tab'],
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'quote-props': ['error', 'as-needed'],
-    'no-trailing-spaces': 'warn',
-    'no-tabs': ['error', { allowIndentationTabs: true }],
-    '@typescript-eslint/require-await': 'warn',
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'google',
+	],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'quote-props': ['error', 'as-needed'],
+		'no-trailing-spaces': 'warn',
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'object-curly-spacing': ['error', 'always'],
+		'@typescript-eslint/require-await': 'warn',
+		'require-jsdoc': 0,
+	},
 }
