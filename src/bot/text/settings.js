a6424795f8d0d9225e117708d75b6a42dca3d72a
Sleroq <cantundo@pm.me>
2022-06-13 11:17:01 +0300

    Renamed "commands" folder to "text"

---
+++ src/bot/text/settings.js
@@ -0,0 +1,26 @@
+const memory = require('../database/data_work.js')
+const { getTranslateTagsText, getshowTagsText } = require('../someFuncs.js')
+
+module.exports.settings_reply = async function (ctx) {
+  let settings = await memory.getSettings(ctx.from.id)
+  if (!settings) {
+    await memory.updateSettings(ctx.from.id)
+    settings = await memory.getSettings(ctx.from.id)
+  }
+  ctx.reply('Settings:', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: getshowTagsText(settings),
+            callback_data: 'showtags',
+          },
+          {
+            text: getTranslateTagsText(settings),
+            callback_data: 'translatetags',
+          },
+        ],
+      ],
+    },
+  })
+}
