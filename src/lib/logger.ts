81dd7355e15ea3334ec88e32ab2c48ddc3447604
Sleroq <cantundo@pm.me>
2022-06-13 21:43:16 +0300

    Logging to file now

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,2 +1,11 @@
 import { pino } from 'pino'
-export default pino()
+const logger = pino(
+	{
+		level: 'info',
+	},
+	pino.destination({
+		dest: './logs/pixiv-bot.log',
+	}),
+)
+
+export default logger
