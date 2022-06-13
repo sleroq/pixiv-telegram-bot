12590d55cd8d510f86c8f3fbb1beeae64ce335f0
Sleroq <cantundo@pm.me>
2022-06-13 12:54:11 +0300

    Refactoring for esm and other new stuff

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,46 @@
-import dotenv from 'dotenv'
-import Werror from './lib/errors'
-import initBot from './bot/index'
-import setWebhook from './server'
+import 'dotenv/config'
+import { pino } from 'pino'
+import Werror from './lib/errors.js'
 
-dotenv.config()
+import initBot from './bot/index.js'
+import setWebhook from './server.js'
 
-const token = process.env.BOT_TOKEN
+const token = process.env['BOT_TOKEN']
 if (!token) {
 	throw new Werror('Bot token not specified')
 }
 
-void (async () => {
-	// try {
-	// 	await connectToSQL()
-	// } catch (error) {
-	// 	throw new Werror(error, 'Connecting to the database')
-	// }
+const logger = pino()
 
-	let bot
+let bot
+try {
+	bot = initBot(token, logger)
+} catch (error) {
+	throw new Werror(error, 'Initializing the bot')
+}
+
+const urlForWebhook = getUrlForWebhook()
+if (urlForWebhook) {
 	try {
-		bot = initBot(token)
+		await setWebhook(bot, urlForWebhook)
 	} catch (error) {
-		throw new Werror(error, 'Initializing the bot')
+		throw new Werror(error, 'Starting with webhook')
 	}
-
-	const urlForWebhook = getUrlForWebhook()
-	if (urlForWebhook) {
-		try {
-			await setWebhook(bot, urlForWebhook)
-		} catch (error) {
-			throw new Werror(error, 'Starting with webhook')
-		}
-		console.log('Webhook is set')
-	} else {
-		console.log('Url for webhook not specified, starting polling')
-		void bot.start({
-			drop_pending_updates: true,
-		})
-	}
-})()
+	logger.info('Webhook is set')
+} else {
+	logger.info('Url for webhook not specified, starting polling')
+	void bot.start({
+		drop_pending_updates: true,
+	})
+}
 
 function getUrlForWebhook() {
-	if (process.env.HEROKU_APP_NAME) {
-		return `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
-	} else if (process.env.REPL_SLUG && process.env.REPL_OWNER) {
-		return `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER.toLowerCase()}.repl.co`
+	if (process.env['HEROKU_APP_NAME']) {
+		return `https://${process.env['HEROKU_APP_NAME']}.herokuapp.com`
+	} else if (process.env['REPL_SLUG'] && process.env['REPL_OWNER']) {
+		return `https://${process.env['REPL_SLUG']}.${process.env[
+			'REPL_OWNER'
+		].toLowerCase()}.repl.co`
 	}
-	return undefined
+	return
 }
